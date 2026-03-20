65453b80bc45a2427ba87d8c5478edaf9e4611bf
s33790 <s33790@pjwstk.edu.pl>
2026-03-20 19:32:38 +0100

    added UserRegistry class

---
+++ RentalService/RentalService/Service/UserRegistry.cs
@@ -0,0 +1,19 @@
+﻿using RentalService.Users;
+
+namespace RentalService.Service;
+
+public class UserRegistry
+{
+    private List<Person> _users = new List<Person>();
+
+    public void AddUser(Person person)
+    {
+        _users.Add(person);
+        Console.WriteLine("User added correctly");
+    }
+
+    // public List<Person> GetAllUsers()
+    // {
+    //     return  _users;
+    // }
+}
